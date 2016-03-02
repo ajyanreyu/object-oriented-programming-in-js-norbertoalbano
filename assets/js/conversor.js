587f0dea25830538a40560bd2a93ac26784e21e0
Albano Yanes <ajyanreyu@gmail.com>
2016-03-02 23:36:57 +0000

    definida funcion media

--- assets/js/conversor.js
+++ assets/js/conversor.js
@@ -1,25 +1,21 @@
 (function(exports) {
   "use strict";
 
-  function Medida(valor,tipo)  
+  function Medida(valor,tipo)
   {
     /* tipo es opcional. Debería admitir  new Medida("45.2 Km") */
     /* ademas de new Medida(45.2, "Km") */
+    this.value = valor;
+    this.type = tipo;
+
   }
+
   
-  function Temperatura(valor,tipo)
-  {
-    /* tipo es opcional. Debería admitir new Medida("45.2 F") */
-  }
-  
-  function Celsius(valor)
-  {
-  }
-  
+
   function Farenheit(valor)
   {
   }
-  
+
   exports.Temperatura = Temperatura;
   exports.Celsius = Celsius;
   exports.Farenheit = Farenheit;
@@ -30,14 +26,14 @@
         /* Extienda la RegeExp a la especificación. use una XRegExp */
         regexp    = /^\s*([-+]?\d+(?:\.\d+)?(?:e[+-]?\d+)?)\s*([a-z,A-Z]+)\s*$/i;
     valor     = valor.match(regexp);
-    
+
     if (valor) {
       var numero = valor[1],
           tipo   = valor[2].toLowerCase();
-      
+
       numero = parseFloat(numero);
       console.log("Valor: " + numero + ", Tipo: " + tipo);
-      
+
       switch (tipo) {
         case 'c':
           var celsius = new Celsius(numero);
@@ -47,7 +43,7 @@
           var farenheit = new Farenheit(numero);
           elemento.innerHTML = farenheit.toCelsius().toFixed(2) + " Celsius";
           break;
-        
+
         default:
           /* rellene este código */
       }
