90f4ca15836ae180661e27a3674f3613f6eeba41
Albano Yanes <ajyanreyu@gmail.com>
2016-03-02 23:49:41 +0000

    terminada clase celcius farenheit y temperatura

--- assets/js/conversor.js
+++ assets/js/conversor.js
@@ -10,11 +10,35 @@
 
   }
 
-  
+  function Temperatura(valor,tipo)
+  {
+    /* tipo es opcional. Debería admitir new Medida("45.2 F") */
+    Media.call(this, valor, tipo);
+  }
+
+  function Celsius(valor)
+  {
+    Temperatura.call(this, valor);
+    this.toFarenheit = function(){
+      return ((this.value * 9/5) + 32);
+    }
+    this.toKelvin = function(){
+      return (this.value + 273.15));
+    }
+  }
+
 
   function Farenheit(valor)
   {
+    this.toCelcius = function(){
+      return (((this.value - 32) * 5)/9);
+    }
+    this.toKelvin = function(){
+      return (((this.value + 459.67) * 5) / 9);
+    }
   }
+
+  
 
   exports.Temperatura = Temperatura;
   exports.Celsius = Celsius;
